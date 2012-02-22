f188ade94a09bf9fbcab7ccbad348a0d780141e2
Durga <Durga@durga-kandasamys-macbook.local>
2012-02-22 14:32:06 -0800

    changes added to test.scala

--- webApp/app/test.scala
+++ webApp/app/test.scala
@@ -5,55 +5,55 @@ import org.apache.http.params.BasicHttpParams
 import org.apache.http.util.EntityUtils
 
 
-import java.sql.Timestamp;
+import java.sql.Timestamp
 
 class AnnotatorClient { 
-	val annotatorUrl = "http://rest.bioontology.org/obs/annotator";
+	val annotatorUrl = "http://rest.bioontology.org/obs/annotator"
 	
-    public static void main( String[] args ) {
+    def main(args: Array[String]) {
         try {
-            HttpClient client = new HttpClient();
+            val httpclient = new HttpClient()
             client.getParams().setParameter(
-            HttpMethodParams.USER_AGENT,"Annotator Client Example - Annotator");  //Set this string for your application 
+            HttpMethodParams.USER_AGENT,"Annotator Client Example - Annotator")  //Set this string for your application 
             
-            PostMethod method = new PostMethod(annotatorUrl);
+            PostMethod method = new PostMethod(annotatorUrl)
             
             // Configure the form parameters
-            method.addParameter("longestOnly","true");
-            method.addParameter("wholeWordOnly","true");
-            method.addParameter("filterNumber", "true");
-            method.addParameter("stopWords","");
-            method.addParameter("withDefaultStopWords","true");
-            method.addParameter("isTopWordsCaseSensitive","false");
-            method.addParameter("mintermSize","3");
-            method.addParameter("scored", "true");
-            method.addParameter("withSynonyms","true"); 
-            method.addParameter("ontologiesToExpand", "");
-            method.addParameter("ontologiesToKeepInResult", ""); 
-            method.addParameter("isVirtualOntologyId", "true"); 
-            method.addParameter("semanticTypes", ""); 
-            method.addParameter("levelMax", "0");
-            method.addParameter("mappingTypes", "null"); //null, Automatic 
-            method.addParameter("textToAnnotate", "Melanoma is a malignant tumor of melanocytes which are found predominantly in skin but also in the bowel and the eye");
-            method.addParameter("format", "xml"); //Options are 'text', 'xml', 'tabDelimited'   
-            method.addParameter("apikey", "YourAPIKey");
+            method.addParameter("longestOnly","true")
+            method.addParameter("wholeWordOnly","true")
+            method.addParameter("filterNumber", "true")
+            method.addParameter("stopWords","")
+            method.addParameter("withDefaultStopWords","true")
+            method.addParameter("isTopWordsCaseSensitive","false")
+            method.addParameter("mintermSize","3")
+            method.addParameter("scored", "true")
+            method.addParameter("withSynonyms","true") 
+            method.addParameter("ontologiesToExpand", "")
+            method.addParameter("ontologiesToKeepInResult", "") 
+            method.addParameter("isVirtualOntologyId", "true") 
+            method.addParameter("semanticTypes", "") 
+            method.addParameter("levelMax", "0")
+            method.addParameter("mappingTypes", "null") //null, Automatic 
+            method.addParameter("textToAnnotate", "Melanoma is a malignant tumor of melanocytes which are found predominantly in skin but also in the bowel and the eye")
+            method.addParameter("format", "xml") //Options are 'text', 'xml', 'tabDelimited'   
+            method.addParameter("apikey", "YourAPIKey")
 
             // Execute the POST method
-            int statusCode = client.executeMethod(method);
+            int statusCode = client.executeMethod(method)
             
             if( statusCode != -1 ) {
                 try {
-                String contents = method.getResponseBodyAsString();
-                method.releaseConnection();
-                System.out.println(contents);
+                String contents = method.getResponseBodyAsString()
+                method.releaseConnection()
+                System.out.println(contents)
                 }
                 catch( Exception e ) {
-                    e.printStackTrace();
+                    e.printStackTrace()
                 }
             }
         }
         catch( Exception e ){
-            e.printStackTrace();
+            e.printStackTrace()
         }
     }
 }
