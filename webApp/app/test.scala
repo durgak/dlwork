4f1b6e9399522718b84d9be2560d66b8c2803286
Lauren Fratamico <lauren.fratamico@gmail.com>
2012-02-22 14:25:47 -0800

    depracated

--- webApp/app/test.scala
+++ webApp/app/test.scala
@@ -1,48 +1,59 @@
-import org.apache.commons.httpclient.*;
-import org.apache.commons.httpclient.methods.*;
-import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.apache.http.client.HttpClient
+import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.params.BasicHttpParams
+import org.apache.http.util.EntityUtils
 
-import java.io.*;
 
-public class HttpClientTutorial {
-  
-  private static String url = "http://www.apache.org/";
+import java.sql.Timestamp;
 
-  public static void main(String[] args) {
-    // Create an instance of HttpClient.
-    HttpClient client = new HttpClient();
+class AnnotatorClient { 
+	val annotatorUrl = "http://rest.bioontology.org/obs/annotator";
+	
+    public static void main( String[] args ) {
+        try {
+            HttpClient client = new HttpClient();
+            client.getParams().setParameter(
+            HttpMethodParams.USER_AGENT,"Annotator Client Example - Annotator");  //Set this string for your application 
+            
+            PostMethod method = new PostMethod(annotatorUrl);
+            
+            // Configure the form parameters
+            method.addParameter("longestOnly","true");
+            method.addParameter("wholeWordOnly","true");
+            method.addParameter("filterNumber", "true");
+            method.addParameter("stopWords","");
+            method.addParameter("withDefaultStopWords","true");
+            method.addParameter("isTopWordsCaseSensitive","false");
+            method.addParameter("mintermSize","3");
+            method.addParameter("scored", "true");
+            method.addParameter("withSynonyms","true"); 
+            method.addParameter("ontologiesToExpand", "");
+            method.addParameter("ontologiesToKeepInResult", ""); 
+            method.addParameter("isVirtualOntologyId", "true"); 
+            method.addParameter("semanticTypes", ""); 
+            method.addParameter("levelMax", "0");
+            method.addParameter("mappingTypes", "null"); //null, Automatic 
+            method.addParameter("textToAnnotate", "Melanoma is a malignant tumor of melanocytes which are found predominantly in skin but also in the bowel and the eye");
+            method.addParameter("format", "xml"); //Options are 'text', 'xml', 'tabDelimited'   
+            method.addParameter("apikey", "YourAPIKey");
 
-    // Create a method instance.
-    GetMethod method = new GetMethod(url);
-    
-    // Provide custom retry handler is necessary
-    method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, 
-    		new DefaultHttpMethodRetryHandler(3, false));
-
-    try {
-      // Execute the method.
-      int statusCode = client.executeMethod(method);
-
-      if (statusCode != HttpStatus.SC_OK) {
-        System.err.println("Method failed: " + method.getStatusLine());
-      }
-
-      // Read the response body.
-      byte[] responseBody = method.getResponseBody();
-
-      // Deal with the response.
-      // Use caution: ensure correct character encoding and is not binary data
-      System.out.println(new String(responseBody));
-
-    } catch (HttpException e) {
-      System.err.println("Fatal protocol violation: " + e.getMessage());
-      e.printStackTrace();
-    } catch (IOException e) {
-      System.err.println("Fatal transport error: " + e.getMessage());
-      e.printStackTrace();
-    } finally {
-      // Release the connection.
-      method.releaseConnection();
-    }  
-  }
+            // Execute the POST method
+            int statusCode = client.executeMethod(method);
+            
+            if( statusCode != -1 ) {
+                try {
+                String contents = method.getResponseBodyAsString();
+                method.releaseConnection();
+                System.out.println(contents);
+                }
+                catch( Exception e ) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        catch( Exception e ){
+            e.printStackTrace();
+        }
+    }
 }
