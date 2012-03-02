1a6a667b1e500daa2b3b454083b6bc73257265ea
Lauren Fratamico <lauren.fratamico@gmail.com>
2012-03-02 14:51:04 -0800

    with querry box

--- webApp/tmp/generated/views.Application.html.query.scala
+++ webApp/tmp/generated/views.Application.html.query.scala
@@ -5,19 +5,17 @@
                     import play.templates.TemplateMagic._
                     import views.html._
 
-                    object index extends BaseScalaTemplate[Html,Format[Html]](HtmlFormat) {
+                    object query extends BaseScalaTemplate[Html,Format[Html]](HtmlFormat) {
 
-                        def apply/*1.2*/(title:String):Html = {
+                        def apply/*1.2*/(query:String):Html = {
                             try {
                                 _display_ {
 
 format.raw/*1.16*/("""
 
-""")+_display_(/*3.2*/main(title = "Home")/*3.22*/ {format.raw/*3.24*/("""
-    <form action="""")+_display_(/*4.20*/action(controllers.Application.queryNCBO))+format.raw/*4.61*/("""" method="GET">
-        <input type="text" name="query" /> 
-        <input type="submit" value="Search NCBO" />
-    </form>
+""")+_display_(/*3.2*/main(title = "Query Results")/*3.31*/ {format.raw/*3.33*/("""
+	<h1>Query text: """)+_display_(/*4.19*/(query ?: "None"))+format.raw/*4.36*/("""</h1>
+	<a href="""")+_display_(/*5.12*/action(controllers.Application.index))+format.raw/*5.49*/("""">Back to search home</a>
 """)})}
                             } catch {
                                 case e:TemplateExecutionError => throw e
@@ -31,10 +29,10 @@ format.raw/*1.16*/("""
                 /*
                     -- GENERATED --
                     DATE: Fri Mar 02 14:49:45 PST 2012
-                    SOURCE: /app/views/Application/index.scala.html
-                    HASH: 14ffb7e46c0816ada3fa031d919f7036c9264c4c
-                    MATRIX: 329->1|450->15|478->18|506->38|526->40|572->60|633->101
-                    LINES: 10->1|14->1|16->3|16->3|16->3|17->4|17->4
+                    SOURCE: /app/views/Application/query.scala.html
+                    HASH: c818561d188cfa826d0c84ef56db330454b139c9
+                    MATRIX: 329->1|450->15|478->18|515->47|535->49|580->68|617->85|660->102|717->139
+                    LINES: 10->1|14->1|16->3|16->3|16->3|17->4|17->4|18->5|18->5
                     -- GENERATED --
                 */
             
