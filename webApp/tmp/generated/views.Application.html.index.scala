3602e316ae58d66df426b9fe1925bc04bea7289f
Lauren Fratamico <lauren.fratamico@gmail.com>
2012-04-30 14:18:38 -0700

    returns jsonobject

--- webApp/tmp/generated/views.Application.html.index.scala
+++ webApp/tmp/generated/views.Application.html.index.scala
@@ -14,11 +14,12 @@
 format.raw/*1.16*/("""
 
 """)+_display_(/*3.2*/main(title = "Home")/*3.22*/ {format.raw/*3.24*/("""
-    <form action="""")+_display_(/*4.20*/action(controllers.Application.queryNCBO))+format.raw/*4.61*/("""" method="GET">
+    <form action="""")+_display_(/*4.20*/action(controllers.Application.reconcile))+format.raw/*4.61*/("""" method="GET">
         <input type="text" name="query" /> 
         <input type="submit" value="Search NCBO" />
     </form>
-""")})}
+""")})+format.raw/*8.2*/("""
+""")}
                             } catch {
                                 case e:TemplateExecutionError => throw e
                                 case e => throw Reporter.toHumanException(e)
@@ -30,11 +31,11 @@ format.raw/*1.16*/("""
                 
                 /*
                     -- GENERATED --
-                    DATE: Fri Mar 02 14:49:45 PST 2012
+                    DATE: Wed Apr 25 15:55:18 PDT 2012
                     SOURCE: /app/views/Application/index.scala.html
-                    HASH: 14ffb7e46c0816ada3fa031d919f7036c9264c4c
-                    MATRIX: 329->1|450->15|478->18|506->38|526->40|572->60|633->101
-                    LINES: 10->1|14->1|16->3|16->3|16->3|17->4|17->4
+                    HASH: e207ed3abd2170e0ff9c7811d3c4a9b98f14ef5f
+                    MATRIX: 329->1|450->15|478->18|506->38|526->40|572->60|633->101|785->226
+                    LINES: 10->1|14->1|16->3|16->3|16->3|17->4|17->4|21->8
                     -- GENERATED --
                 */
             
